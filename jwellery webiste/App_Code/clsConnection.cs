b5f8a8eebf01198aace9a296a5df75b18e658795
Madhvi Agrawal <nidhi.agrawal414@gmail.com>
2016-05-11 23:22:30 +0530

    Connection Class

---
+++ jwellery webiste/App_Code/clsConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+/// <summary>
+/// Summary description for clsConnection
+/// </summary>
+public class clsConnection
+{
+ 
+   public static SqlConnection Connect()
+   {
+       SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+       try
+       {
+           con.Open();
+       }
+       catch
+       {
+           con.Close();
+       }
+       return con;
+
+   }
+	public clsConnection()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+   
+}
