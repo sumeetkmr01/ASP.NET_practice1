d23127b30ed6481fe53e67a4f14b811f89e9e666
Madhvi Agrawal <nidhi.agrawal414@gmail.com>
2016-05-23 22:44:12 +0530

    After DashBoard Design

--- jwellery webiste/login.aspx
+++ jwellery webiste/login.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="login.aspx.cs" Inherits="login" %>
+﻿    <%@ Page Language="C#" AutoEventWireup="true" CodeFile="login.aspx.cs" Inherits="login" %>
 
 <!DOCTYPE html>
 
@@ -11,31 +11,36 @@
    
     <title>
     </title>
-     <%--<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>--%>
+    <%-- <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>--%>
     <script type="text/javascript">
-        $(function () {
-            $("[id*=btnSubmit]").bind("click", function () {
-                var user = {};
-                user.Username = $("[id*=txtEmail]").val();
-                user.Password = $("[id*=txtPassword]").val();
-                $.ajax({
-                    type: "POST",
-                    url: "login.aspx/ValidateUser",
-                    data: '{user: ' + JSON.stringify(user) + '}',
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json",
-                    success: function (response) {
-                        alert(response.d);
-                    }
-                });
-                return false;
-            });
-        });
+        
+        //    function onlogin() {
+              
+        //        var user = {};
+        //        user.Username = $("#txtEmail").val();
+        //        user.Password = $("#txtPassword").val();
+        //        var formData = new FormData();
+        //        formData.append('IdentificationId', 0);
+        //        alert("a");
+        //        $.ajax({
+        //            type: 'GET',
+        //            url: "http://localhost:2468/proj/login.aspx/Authenticate",
+        //            data: '',
+        //            processData: false,
+        //            contentType: "application/json; charset=utf-8",
+        //            cash: false,
+        //            success: function (result) {
+        //                if (result.Error != undefined) {
+
+        //                }
+                      
+        //            }
+        //       });     
+        //}
 </script>
 </head>
 <body>
     <div class="Container">
-<%--<form id="form1" runat="server">--%>
     <div class="row">
        <div class="col-md-4 col-md-offset-4">
            <div class="login-panel panel panel-default">
@@ -43,21 +48,25 @@
                    <h3 class="panel-title" >Please Sign In</h3>
               </div>
                 <div class="panel-body">
-                   <form role="form">
+                    <form id="form1" runat="server" role="form">
+                   
                   <fieldset>
                    <div class="form-group">
-                  <input class="form-control" placeholder="E-mail" name="Email"  id="txtEmail"  runat="server"/>
+                  <%--<input class="form-control" placeholder="E-mail" name="Email" runat="server"  id="txtEmail" />--%>
+                       <asp:TextBox runat="server"  id="txtEmail" placeholder="E-mail" name="Email" class="form-control"></asp:TextBox>
                </div>
                       <div class="form-group">
-                          <input class="form-control" placeholder="Password" type="password" name="Password" id="txtPassword" runat="server"/>
+                          <%--<input class="form-control" placeholder="Password" type="password" name="Password" id="txtPassword"  runat="server"/>--%>
+                          <asp:TextBox placeholder="Password" type="password" name="Password" id="txtPassword"  runat="server" class="form-control"></asp:TextBox>
                       </div>
                       <div class="checkbox">
                            <label>
                           <input type="checkbox" name="Remember"  value="Remembeer Me" />Remember Me
                      </label>
                                 </div>
-                       <button  id="btnSubmit" class ="btn btn-lg btn-success btn-block"  runat="server" >Submit</button>
-                      
+                    <button  id="btnSubmit" class ="btn btn-lg btn-success btn-block" onserverclick="btnSubmit_Click" runat="server" >Submit</button>
+                  <%--<asp:Button ID ="v" OnClick="v_Click"--%>
+                   
                        </fieldset>
               </form>
                 </div>   
@@ -65,7 +74,7 @@
            </div>
        </div>
     </div>
-    <%--</form>--%>
+
     </div>
     
 </body>
--- jwellery webiste/login.aspx.cs
+++ jwellery webiste/login.aspx.cs
@@ -7,53 +7,114 @@ using System.Web.UI.WebControls;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Data;
+using System.Web.Services;
+using System.Web.Script.Services;
 
 public partial class login : System.Web.UI.Page
 {
+    string query = "", username = "", password = "";
+    int id = 0;
     protected void Page_Load(object sender, EventArgs e)
     {
-        SqlCommand co = new SqlCommand();
-        co.Connection = clsConnection.Connect();
+        //SqlCommand co = new SqlCommand();
+        //co.Connection = clsConnection.Connect();
     }
-
-    [System.Web.Services.WebMethod]
-    [System.Web.Script.Services.ScriptMethod]
-    private static string ValidateUser(clsUsers user)
+    protected void btnSubmit_Click(object sender, EventArgs e)
     {
-        int userId = 0;
-       // string constr = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
-        //using (SqlConnection con = new SqlConnection(clsConnection.Connect()))
-        //{
-            using (SqlCommand cmd = new SqlCommand("Validate_Admin"))
-            {
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Username", user.Username);
-                cmd.Parameters.AddWithValue("@Password", user.Password);
-                cmd.Connection = clsConnection.Connect();
-                //con.Open();
-                userId = Convert.ToInt32(cmd.ExecuteScalar());
-                //con.Close();
-            }
-        //}
-        string IsValidate = string.Empty;
-        switch (userId)
+        query = "SELECT ID,UserName,Password FROM Login WHERE USERNAME=@user AND PASSWORD=@psd";
+        SqlCommand com = new SqlCommand();
+        com.Connection = clsConnection.Connect();
+        com.CommandType = CommandType.Text;
+        com.CommandText = query;
+        com.Parameters.AddWithValue("@user",txtEmail.Text.Trim());
+        com.Parameters.AddWithValue("@psd", txtPassword.Text.Trim());
+        SqlDataReader dr = com.ExecuteReader();
+        if (dr.Read())
         {
-            case -1:
-                IsValidate = "Username and/or password is incorrect.";
-                break;
-            case -2:
-                IsValidate = "Account has not been activated.";
-                break;
-            case -3:
-                IsValidate = "Login Successfully.";
-                break;
+            id = Convert.ToInt32(dr[0].ToString().Trim());
+            username = dr[1].ToString().Trim();
+            password = dr[2].ToString().Trim();
+            //Response.Redirect("~/");
         }
+        else if (txtEmail.Text != username)
+        {
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "key", "alert('Invalid Username');", true);
+        }
+        else if(txtPassword.Text!=password)
+        {
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "key", "alert('Invalid Password');", true);
+        }
+        //Open();
+    }
 
-        return IsValidate;
-    }
-    protected void btnSubmit_ServerClick(object sender, EventArgs e)
+    private void Open()
     {
-       // ValidateUser(a);
+        if (HttpContext.Current.CurrentHandler is Page)
+        {
+            string QR_str = "", title="";
+            string fil_loc = System.Web.VirtualPathUtility.ToAbsolute("~/PopUpPage.aspx");
+            //fil_loc = "SSeek.aspx";
+
+            Page p = (Page)HttpContext.Current.CurrentHandler;
+
+            if (ScriptManager.GetCurrent(p) != null)
+            {
+                ScriptManager.RegisterClientScriptBlock(p, p.GetType(), "PopUP", "OpenSingle('" + fil_loc + "?STR=" + QR_str + "','900px','490px','" + title + "');", true);
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(p, p.GetType(), "PopUP", "OpenSingle('" + fil_loc + "','900px','490px','" + title + "');", true);
+            }
+        }
     }
-   
+
+
+    #region Comment
+    //    [WebMethod] 
+//[ScriptMethod(UseHttpGet = true)]
+//public static bool Authenticate(int username, string password)
+//{
+//    bool isAuthenticated = false; // some method to authenticate
+//   return isAuthenticated;
+//}
+
+
+    //[WebMethod]
+    //private static string ValidateUser()
+    //{
+ 
+    //    int userId = 0;
+    //   // string constr = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+    //    //using (SqlConnection con = new SqlConnection(clsConnection.Connect()))
+    //    //{
+    //        using (SqlCommand cmd = new SqlCommand("Validate_Admin"))
+    //        {
+    //            cmd.CommandType = CommandType.StoredProcedure;
+    //            //cmd.Parameters.AddWithValue("@Username", user.Username);
+    //            //cmd.Parameters.AddWithValue("@Password", user.Password);
+    //            cmd.Connection = clsConnection.Connect();
+    //            //con.Open();
+    //            userId = Convert.ToInt32(cmd.ExecuteScalar());
+    //            //con.Close();
+    //        }
+    //    //}
+    //    string IsValidate = string.Empty;
+    //    switch (userId)
+    //    {
+    //        case -1:
+    //            IsValidate = "Username and/or password is incorrect.";
+    //            break;
+    //        case -2:
+    //            IsValidate = "Account has not been activated.";
+    //            break;
+    //        case -3:
+    //            IsValidate = "Login Successfully.";
+    //            break;
+    //    }
+
+    //    return IsValidate;
+    //}
+
+    #endregion
+    
 }
